1e16c69bc8258c23eb7d91d23c55e03fc8ca1129
Megane <fulconis.megane@gmail.com>
2019-11-01 00:45:20 +0100

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,77 +7,55 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 Faker::Config.locale = :fr
-
-#User.destroy.all
-#Gossip.destroy.all
-
+p "#####################################"
+p "DESTROY ALL"
+User.destroy_all
+p "USER DESTROY"
+Gossip.destroy_all
+p"GOSSIP DESTROY"
+p "#####################################"
+5.times do      #cree 10 villes
+    City.create!(name: Faker::Address.city, zip_code: Faker::Address.zip)
+end
+p "database has now #{City.count} cities"
+5.times do      #cree 10 user avec une ville en reference
+    User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Quote.famous_last_words, email: Faker::Internet.email, age: rand(18..99), city: City.all.sample, password: Faker::Creature::Animal.name)
+end
+p "database has now #{User.count} users"
 5.times do
-  name = Faker::FunnyName.unique.two_word_name
-  name = name.split(' ')
-  description = Faker::TvShows::RuPaul.quote
-  email = Faker::Internet.unique.safe_email(name: name)
-  age = rand(16..70)
-  password = Faker::Internet.password
-  User.create(
-  	first_name: name[0], 
-  	last_name: name[1],
-  	description: description, 
-  	email: email, 
-  	age: age,
-  	city_id: City.all.sample.id,
-    password_digest: password
-  	)
+  gossip = Gossip.create!(
+    title: Faker::Creature::Animal.name,
+    content: Faker::Quote.most_interesting_man_in_the_world,
+    user_id: User.all.sample.id)
 end
+p "database has now #{Gossip.count} gossips"
 
-puts "ok"
-
-
-5.times do		#cree 10 villes
-	City.create!(name: Faker::Address.city, zip_code: Faker::Address.zip)
-end
-
-puts "ok"
-
-5.times do
-  title = Faker::Book.unique.title
-  sentences = rand(3..8)
-  content = Faker::TvShows::NewGirl.quote
-  user = rand(1..10)
-  Gossip.create(
-  	title: title, 
-  	content: content, 
-  	user_id: User.all.sample.id
-  	)
-end
-
-puts "ok"
-
-#
-#5.times do 		#cree 10 tag
-#	Tag.create!(title: "##{Faker::Games::Pokemon.name}")
+#5.times do      #cree 10 tag
+#    Tag.create!(title: "##{Faker::Games::Pokemon.name}")
 #end
-#
-##1.upto(20) do |index|		#Fait la relation de 20 gossips avec des tags
-##	TagList.create(gossip: Gossip.find(index), tag: Tag.all.sample)	
+#p "database has now #{Tag.count} tags"
+##1.upto(20) do |index|      #Fait la relation de 20 gossips avec des tags
+##   TagList.create(gossip: Gossip.find(index), tag: Tag.all.sample) 
 ##end
-#
-#5.times do	#creer 20 messages en reference avec l'utilsateur sender et creer un nombre aleatoire de receveur user
-#	message = PrivateMessage.create(sender: User.all.sample, content: Faker::Movies::HarryPotter.quote)
-#	rand(1..6).times do
-#		Recipient.create(private_message: message, user: User.all.sample)
-#	end
+#5.times do  #creer 20 messages en reference avec l'utilsateur sender et creer un nombre aleatoire de receveur user
+#    message = PrivateMessage.create!(sender: User.all.sample, content: Faker::Movies::HarryPotter.quote)
+#    rand(1..6).times do
+#        Recipient.create!(private_message: message, user: User.all.sample)
+#    end
 #end
-#
-#5.times do		#cree 20 comments
-#	Comment.create!(content: Faker::Movies::Hobbit.quote, user: User.all.sample, gossip: Gossip.all.sample)
+#p "database has now #{PrivateMessage.count} private messages"
+#5.times do      #cree 20 comments
+#    Comment.create!(content: Faker::Movies::Hobbit.quote, user: User.all.sample, gossip: Gossip.all.sample)
 #end
-#
-#5.times do 		#creer 20 like qui est de maniere aleatoire un like de comment ou de gossip
-#	if rand(0..10)%2==0 
-#		Like.create(user: User.all.sample, gossip: Gossip.all.sample)
-#	else
-#		Like.create(user: User.all.sample, comment: Comment.all.sample)
-#	end
+#p "database has now #{Comment.count} comments"
+#5.times do      #creer 20 like qui est de maniere aleatoire un like de comment ou de gossip
+#    if rand(0..10)%2==0 
+#        Like.create!(user: User.all.sample, gossip: Gossip.all.sample)
+#    else
+#        Like.create!(user: User.all.sample, comment: Comment.all.sample)
+##    end
 #end
-
+#p "database has now #{Like.count} likes"
+p "#############################################"
+p "END OF SEED"
 puts "all entries added"
